795166cd52d62149905cc794863e1f6e103d2fbe
John Knotts <jhnnyk@gmail.com>
2019-09-22 16:48:49 -0600

    getting input from users

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const chalk = require('chalk');
 const getNotes = require('./notes');
 
-const msg = getNotes();
-console.log(msg);
+const command = process.argv[2];
 
-console.log(chalk.bgMagenta.bold('Success!'));
+if (command === 'add') {
+  console.log('Adding note!');
+} else if (command === 'remove') {
+  console.log('Removing note!');
+}

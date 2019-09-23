6d175071ecd22d06ef61ab09abbaba56292e2e8b
John Knotts <jhnnyk@gmail.com>
2019-09-23 14:27:30 -0600

    argument parsing with Yargs part 2

--- app.js
+++ app.js
@@ -9,8 +9,20 @@ yargs.version('0.1.0');
 yargs.command({
   command: 'add',
   describe: 'Add a new note',
-  handler: function() {
-    console.log('Adding a new note');
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note content',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv) {
+    console.log('Title: ' + argv.title + '\n' + 'Content: ' + argv.body);
   }
 });
 
@@ -41,4 +53,4 @@ yargs.command({
   }
 });
 
-console.log(yargs.argv);
+yargs.parse();

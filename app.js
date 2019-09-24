86a77c69037a834bada42bdf8b23a11ed017034f
John Knotts <jhnnyk@gmail.com>
2019-09-24 09:25:38 -0600

    adding a note

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 const yargs = require('yargs');
-const getNotes = require('./notes');
+const notes = require('./notes');
 
 // Customize yargs version
 yargs.version('0.1.0');
@@ -22,7 +22,7 @@ yargs.command({
     }
   },
   handler: function(argv) {
-    console.log('Title: ' + argv.title + '\n' + 'Content: ' + argv.body);
+    notes.addNote(argv.title, argv.body);
   }
 });
 

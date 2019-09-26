79d9daf120f427f1ec0dfb22b82d0b7dfb930f9a
John Knotts <jhnnyk@gmail.com>
2019-09-26 10:32:43 -0600

    reading a note

--- notes.js
+++ notes.js
@@ -1,10 +1,6 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
-const getNotes = () => {
-  return 'Your notes...';
-};
-
 const addNote = (title, body) => {
   const notes = loadNotes();
 
@@ -43,6 +39,17 @@ const listNotes = () => {
   });
 };
 
+const readNote = title => {
+  const notes = loadNotes();
+  const note = notes.find(note => note.title === title);
+  if (note) {
+    console.log(chalk.blue.bold(note.title));
+    console.log(note.body);
+  } else {
+    console.log(chalk.bgRed('Error: note not found!'));
+  }
+};
+
 const saveNotes = notes => {
   const dataJSON = JSON.stringify(notes);
   fs.writeFileSync('notes.json', dataJSON);
@@ -59,8 +66,8 @@ const loadNotes = () => {
 };
 
 module.exports = {
-  getNotes: getNotes,
   addNote: addNote,
   removeNote: removeNote,
-  listNotes: listNotes
+  listNotes: listNotes,
+  readNote: readNote
 };
